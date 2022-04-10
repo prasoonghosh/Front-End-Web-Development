de695e1aed3ae26de23668adf0d5c23fb7761c17
prasoonghosh <prasoonpg.ghosh@gmail.com>
2022-04-10 18:25:13 +0530

    Commiting files

---
+++ Test-2/js_test2.js
@@ -0,0 +1,96 @@
+function myFunction(a) {
+    if (a == 1) {
+        document.getElementById("Dehradun").style.display = "block";
+        document.getElementById("Delhi").style.display = "none";
+        document.getElementById("Mohali").style.display = "none";
+        document.getElementById("Shimla").style.display = "none";
+    }
+    else if (a == 2) {
+        document.getElementById("Dehradun").style.display = "none";
+        document.getElementById("Delhi").style.display = "block";
+        document.getElementById("Mohali").style.display = "none";
+        document.getElementById("Shimla").style.display = "none";
+    }
+    else if (a == 3) {
+        document.getElementById("Dehradun").style.display = "none";
+        document.getElementById("Delhi").style.display = "none";
+        document.getElementById("Mohali").style.display = "block";
+        document.getElementById("Shimla").style.display = "none";
+    }
+    else {
+        document.getElementById("Dehradun").style.display = "none";
+        document.getElementById("Delhi").style.display = "none";
+        document.getElementById("Mohali").style.display = "none";
+        document.getElementById("Shimla").style.display = "block";
+    }
+}
+
+function validateForm() {
+
+    var finame = document.forms["form"]["fname"].value;
+    var laname = document.forms["form"]["lname"].value;
+    var emailAdd = document.forms["form"]["email"].value;
+    var phoneNo = document.forms["form"]["pnumber"].value;
+    if (finame == "") {
+        alert("Plese enter the First Name");
+        document.getElementById("fname").style.backgroundColor = "lightyellow";
+    }
+    if (laname == "") {
+        alert("Plese enter the Last Name");
+        document.getElementById("lname").style.backgroundColor = "lightyellow";
+    }
+    if (emailAdd == "") {
+        alert("Plese enter the Email Address");
+        document.getElementById("email").style.backgroundColor = "lightyellow";
+    }
+    if (phoneNo == " ") {
+        alert("Plese enter the Phone number");
+        document.getElementById("pnumber").style.backgroundColor = "lightyellow";
+    }
+}
+function changeColourFname(){
+    document.getElementById("fname").style.background = "yellow";
+}
+function changeColourLname(){
+    document.getElementById("lname").style.background = "yellow";
+}
+function changeColourPhone(){
+    document.getElementById("pnumber").style.background = "yellow";
+}
+function changeColourEmail(){
+    document.getElementById("email").style.background = "yellow";
+}
+function blurBack(){
+    document.getElementById("fname").style.background = "";
+    document.getElementById("lname").style.background = "";
+    document.getElementById("pnumber").style.background = "";
+    document.getElementById("email").style.background = "";
+    
+
+}
+function changefont(){
+    const a= document.getElementById("fname");
+    a.value =a.value.toUpperCase();
+    const b= document.getElementById("lname");
+    b.value =b.value.toUpperCase();
+    const c= document.getElementById("pnumber");
+    c.value =c.value.toUpperCase();
+    const d= document.getElementById("email");
+    d.value =d.value.toUpperCase();
+}
+function prompt(){
+    var val;
+  if (confirm("You want to Submit? ")) {
+    val = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("submit").innerHTML = val;
+}
+var date = new Date(); 
+var day= date.getDate();
+var month= date.getMonth();
+var year= date.getFullYear();
+var hour=date.getHours();
+var min=date.getMinutes();
+document.getElementById("time").innerHTML = day+'-'+month+'-'+year+" "+" "+hour+":"+min+" ";
