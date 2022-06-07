2c8c14c0545d4ad5b1b54dd95aaf681ac74c2d29
prasoonghosh <prasoonpg.ghosh@gmail.com>
2022-06-07 13:37:23 +0530

    Learned Event Handling

--- Practice/js/scripts.js
+++ Practice/js/scripts.js
@@ -2,27 +2,42 @@
 // console.log(document.getElementById("title"));
 // console.log(document instanceof HTMLDocument);
 
-function sayHello() {
-    var name =
-        document.getElementById("name").value;
-        var message = "<h2>Hello " + name + "!</h2>";
+document.addEventListener("DOMContentLoaded",
+    function (event){
+        function sayHello(event) {
+            this.textContent = "Said it!"
+            var name =
+                document.getElementById("name").value;
+            var message = "<h2>Hello " + name + "!</h2>";
 
 //     document
 //         .getElementById("content")
 //         .textContent = message;
 // }
-    document
-        .getElementById("content")
-        .innerHTML = message;
-
-    if (name === "student"){
-        var title =
             document
-                .querySelector("#title")
-                .textContent;
-        title += " & loving it!";
-        document
-            .querySelector("#title")
-            .textContent = title;
+                .getElementById("content")
+                .innerHTML = message;
+
+            if (name === "student"){
+                var title =
+                    document
+                        .querySelector("#title")
+                        .textContent;
+                title += " & loving it!";
+                document
+                    .querySelector("#title")
+                    .textContent = title;
+            }
+        }
+
+        document.querySelector("button")
+            .addEventListener("click", sayHello);
+
     }
-}
+    );
+
+
+
+
+// document.querySelector("button")
+//     .onclick = sayHello;
